42acebfef4c55b778c5fbec51e22caec13088b89
Ian Chen <ichen2@lsu.edu>
2021-06-19 14:45:52 -0500

    Made script text me if it finds a ps5 using Twilio

--- main.py
+++ main.py
@@ -2,6 +2,12 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException        
 import time
+import os
+from twilio.rest import Client
+
+account_sid = os.environ['TWILIO_ACCOUNT_SID']
+auth_token = os.environ['TWILIO_AUTH_TOKEN']
+client = Client(account_sid, auth_token)
 
 options = webdriver.ChromeOptions()
 options.add_argument("headless")
@@ -14,16 +20,20 @@ BEST_BUY_URL = "https://www.bestbuy.com/site/sony-playstation-5-console/6426149.
 found_at_target = False
 found_at_best_buy = False
 
-def ps5Found():
+def isFound():
+    global found_at_best_buy
+    global found_at_target
     return found_at_target or found_at_best_buy
 
 def checkTarget():
+    global found_at_target
     print("Checking Target...")
     driver.get(TARGET_URL)
     found_at_target = "Sold out" not in driver.page_source
     print("Found at Target!" if found_at_target else "Not Found")
 
 def checkBestBuy():
+    global found_at_best_buy
     print("Checking Best Buy...")
     driver.get(BEST_BUY_URL)
     try:
@@ -32,7 +42,15 @@ def checkBestBuy():
     except NoSuchElementException:
         print("Found at BestBuy!")
 
-while not ps5Found():
+while not isFound():
     checkTarget()
     checkBestBuy()
     time.sleep(2)
+
+
+message = client.messages \
+                .create(
+                     body="PS5 Found!! " + TARGET_URL if found_at_target else BEST_BUY_URL,
+                     from_=os.environ['TWILIO_PHONE_NUMBER'],
+                     to=os.environ['MY_PHONE_NUMBER']
+                 )
